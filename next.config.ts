f083cf0ccdb7227513b7bfa41331c17fe27aacbe
Sean Brill <sean@Seans-MacBook-Pro.local>
2026-02-27 13:17:51 -0500

    Configure basepath for GitHub Pages subpath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  // basePath: '/four-seasons-wharton', // Uncomment and set to repository name if deploying to a GitHub Pages project subpath
+  basePath: '/4SeasonsWharton',
   images: {
     unoptimized: true, // Required for static export
     formats: ['image/webp'],
